eb4dd1a7001b97218233500c14b078bff3c12ac2
Saikrishna Madupu <krishh782@gmail.com>
2018-07-23 21:35:46 -0400

    message

--- ec2.tf
+++ ec2.tf
@@ -6,3 +6,4 @@ resource "aws_instance" "terraform-vault" {
   subnet_id              = "${aws_subnet.public.id}"
   vpc_security_group_ids = ["${aws_security_group.sgroup.id}"]
   user_data              = "${var.user_data}"
+ }
